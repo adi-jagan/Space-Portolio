313699aa9ce97373f770dfd1c0c9026d1d727725
Adi Jagnnathan <Aditya.Jagnnathan@gmail.com>
2023-12-23 23:18:20 -0800

    new portfolio repo clone and update

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,9 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
-        <Encryption />
         <Projects />
+        <Encryption />
+
       </div>
     </main>
   );
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -23,9 +23,7 @@ const HeroContent = () => {
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
-          </h1>
+          <h1 className="Welcome-text text-[13px]"> Venture Studio </h1>
         </motion.div>
 
         <motion.div
@@ -33,12 +31,11 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            We help knowledge workers
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              build momentum.{" "}
             </span>
-            project exprience
           </span>
         </motion.div>
 
@@ -46,8 +43,7 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          We help build meaningful ventures that solve real-world knowledge management and re-skilling problems for knowledge workers.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
